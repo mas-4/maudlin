d69e12392a34bf8a183551e23e6363e2f8fb301b
malan88 <m@stdwtr.io>
2021-05-07 14:29:53 -0400

    Add nbc

---
+++ newscrawler/spiders/nbc.py
@@ -0,0 +1,40 @@
+import scrapy
+from dateutil import parser
+from bs4 import BeautifulSoup as BS
+from newscrawler.mixins import BoilerPlateParser
+
+
+class NbcSpider(scrapy.Spider, BoilerPlateParser):
+    name = 'nbc'
+    allowed_domains = ['www.nbcnews.com']
+    start_urls = ['https://www.nbcnews.com/']
+
+    def parse(self, response):
+        soup = BS(response.text, 'lxml')
+        if response.url != self.start_urls[0]:
+            item = self.prepopulate_item(response)
+
+            item['title'] = soup.find('h1', class_='article-hero__headline').text.strip()
+            item['byline'] = soup.find('div', class_='article-byline__name').text.strip()
+
+            date = soup.find('time')['datetime'].strip()
+            date = parser.parse(date)
+            item['date'] = date
+
+            story = soup.find('div', class_='article-body__content')
+            paragraphs = story.find_all('p')
+            text = self.joinparagraphs(paragraphs)
+            item['text'] = text.replace('\xa0', ' ')
+
+            yield item
+
+        if response.url == self.start_urls[0]:
+            root = soup.find('div', class_='layout-grid-item')
+            links = []
+            for a in root.find_all('a'):
+                try:
+                    links.append(a['href'])
+                except:
+                    continue
+            for link in links:
+                yield response.follow(link, callback=self.parse)
