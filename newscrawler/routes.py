8ba525c8a943141645424e254907cb8a02277137
malan88 <m@stdwtr.io>
2021-05-07 12:40:13 -0400

    Add index

--- newscrawler/routes.py
+++ newscrawler/routes.py
@@ -1,7 +1,8 @@
+from flask import render_template
 from newscrawler import app
 from newscrawler.models import Article
 
 @app.route('/')
 def index():
     count = Article.query.count()
-    return str(count)
+    return render_template('index.html', count=count)
