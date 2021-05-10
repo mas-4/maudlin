bc6606e4d8213e429020c0b6af1d6f8b1748489b
malan88 <m@stdwtr.io>
2021-05-10 16:41:30 -0400

    Fix scrapy ignoring dotenv

--- newscrawler/__init__.py
+++ newscrawler/__init__.py
@@ -1,12 +1,13 @@
-import logging
+import os, logging
 from logging.handlers import RotatingFileHandler
-import os
+from dotenv import load_dotenv
 from flask import Flask
 from flaskext.markdown import Markdown
 from flask_sqlalchemy import SQLAlchemy
 from newscrawler.config import Config
 from newscrawler import utils
 
+load_dotenv()
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
@@ -26,9 +27,9 @@ if not app.debug:
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
 
-    app.logger.setLevel(logging.INFO)
-    app.logger.info('Newscrawler startup')
-    app.logger.info(Config.SQLALCHEMY_DATABASE_URI)
+app.logger.setLevel(logging.INFO)
+app.logger.info('Newscrawler startup')
+app.logger.info(Config.SQLALCHEMY_DATABASE_URI)
 
 app.jinja_env.globals.update(len=len)
 app.jinja_env.globals.update(color=utils.color)
