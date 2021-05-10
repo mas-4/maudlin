7631d2bbff101f0ccfc51a44445e5b508a20398a
malan88 <m@stdwtr.io>
2021-05-10 15:59:01 -0400

    Add huffpost

--- newscrawler/spiders/huffpost.py
+++ newscrawler/spiders/huffpost.py
@@ -5,16 +5,13 @@ from bs4 import BeautifulSoup as BS
 from newscrawler.mixins import BoilerPlateParser
 
 headers = {
-    'User-Agent': 'python-requests/2.25.1',
-    'Accept-Encoding': 'gzip, deflate',
-    'Accept': '*/*',
-    'Connection': 'keep-alive'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
 }
 
-class WapoSpider(scrapy.Spider, BoilerPlateParser):
-    name = 'wapo'
-    allowed_domains = ['www.washingtonpost.com']
-    start_urls = ['https://www.washingtonpost.com/']
+class HuffpostSpider(scrapy.Spider, BoilerPlateParser):
+    name = 'huffpost'
+    allowed_domains = ['www.huffpost.com']
+    start_urls = ['https://www.huffpost.com/']
 
     def start_requests(self):
         return [scrapy.Request(self.start_urls[0],
@@ -26,23 +23,24 @@ class WapoSpider(scrapy.Spider, BoilerPlateParser):
         if response.url != self.start_urls[0]:
             item = self.prepopulate_item(response)
 
-            item['title'] = soup.find('h1', id='main-content').text.strip()
+            item['title'] = soup.find(re.compile(r'h1|p'), attrs={'class': re.compile(r'headline')}).text.strip()
 
-            byline = soup.find('span', attrs={'data-sc-c': 'author'})
-            item['byline'] = byline.text.strip()
+            byline = soup.find('div', attrs={'class': re.compile(r'byline')})
+            item['byline'] = byline.text.strip() if byline else None
 
-            date = soup.find('div', attrs={'data-qa':'timestamp'}).text.strip()
+            date = soup.find('div', attrs={'class':'timestamp'})\
+                .span.text.strip().replace('ET', 'EDT')
             date = parser.parse(date)
             item['date'] = date
 
-            article = soup.find('div', class_='article-body')
+            article = soup.find('div', {'class': re.compile('entry__content')})
             paragraphs = article.find_all('p')
             text = self.joinparagraphs(paragraphs)
             item['text'] = text.replace('\xa0', ' ')
             yield item
 
         if response.url == self.start_urls[0]:
-            attrs={'href': re.compile(r'^https://www.washingtonpost.com/.*story\.html$')}
+            attrs={'href': re.compile(r'/entry/')}
             links = set(a['href'] for a in soup.find_all('a', attrs=attrs))
             for link in links:
                 yield response.follow(link, callback=self.parse, headers=headers)
