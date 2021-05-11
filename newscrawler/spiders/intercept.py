56520e15d93918db2bcdead8ff1edd2acad3f820
malan88 <m@stdwtr.io>
2021-05-11 10:59:04 -0400

    Add intercept

---
+++ newscrawler/spiders/intercept.py
@@ -0,0 +1,35 @@
+import re
+import scrapy
+from bs4 import BeautifulSoup as BS
+from newscrawler.mixins import BoilerPlateParser
+from dateutil import parser
+
+
+class InterceptSpider(scrapy.Spider, BoilerPlateParser):
+    name = 'intercept'
+    allowed_domains = ['theintercept.com']
+    start_urls = ['https://theintercept.com/']
+
+    def parse(self, response):
+        soup = BS(response.text, 'lxml')
+        if response.url != self.start_urls[0]:
+            item = self.prepopulate_item(response)
+
+            item['title'] = soup.find('h1', class_='Post-title').text.strip()
+            byline = soup.find('div', class_='PostByline-names')
+            item['byline'] = re.sub('  +', ' ', byline.text.strip())
+
+            date = soup.find('span', 'PostByline-date').text.strip()
+            date = parser.parse(re.sub('  +', ' ', date))
+            item['date'] = date
+
+            root = soup.find('div', class_='PostContent')
+            paragraphs = root.find_all('p')
+            item['text'] = self.joinparagraphs(paragraphs)
+            yield item
+
+        if response.url == self.start_urls[0]:
+            attrs = {'href': re.compile(r'^/\d{4}/\d{2}/\d{2}/')}
+            links = set(a['href'] for a in soup.find_all('a', attrs=attrs))
+            for link in links:
+                yield response.follow(link, callback=self.parse)
