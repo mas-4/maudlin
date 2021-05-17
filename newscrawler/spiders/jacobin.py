1b088386ba7d2b66275163cb8a6d0e7c1c1843ed
malan88 <m@stdwtr.io>
2021-05-17 09:09:10 -0400

    Add jacobin

---
+++ newscrawler/spiders/jacobin.py
@@ -0,0 +1,49 @@
+import re
+import scrapy
+from bs4 import BeautifulSoup as BS
+from newscrawler.mixins import BoilerPlateParser
+from dateutil import parser
+
+
+class JacobinSpider(scrapy.Spider, BoilerPlateParser):
+    name = 'jacobin'
+    allowed_domains = ['jacobinmag.com']
+    start_urls = ['https://jacobinmag.com/']
+
+    def parse(self, response):
+        soup = BS(response.text, 'lxml')
+
+        if response.url != self.start_urls[0]:
+            item = self.prepopulate_item(response)
+
+            tag = 'h1'
+            attrs = {'class': 'po-hr-cn__title'}
+            title = soup.find(tag, attrs=attrs)
+            title = title.text.strip()
+
+            tag = 'dd'
+            attrs = {'class': 'po-hr-cn__author'}
+            byline = soup.find(tag, attrs=attrs)
+            byline = re.sub('  +', ' ', byline.text.strip()) if byline else None
+
+            tag = 'time'
+            attrs = {'class': 'po-hr-fl__date'}
+            date = soup.find(tag, attrs)
+            date = date.text.strip()
+            date = parser.parse(date)
+
+            tag = 'div'
+            attrs = {'id': 'post-content'}
+            root = soup.find(tag, attrs=attrs)
+            paragraphs = root.find_all('p')
+            text = self.joinparagraphs(paragraphs)
+
+
+            item = self.populate_item(title, byline, date, text, item)
+            yield item
+
+        if response.url == self.start_urls[0]:
+            attrs = {'href': re.compile(r'/\d{4}/\d{2}/')}
+            links = set(a['href'] for a in soup.find_all('a', attrs=attrs))
+            for link in links:
+                yield response.follow(link, callback=self.parse)
